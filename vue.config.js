1afbdb9421dc95bb9acbaeb7e3adb48fc9a4700c
liyuan <liyuan520310@163.com>
2020-05-11 20:38:30 +0800

    构建时输出：时间/环境变量/内部版本号

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// console.log('env:\n', process.env)
+
+console.log('ENV:', process.env.ENV)
+console.log('NODE_ENV:', process.env.NODE_ENV)
+console.log('VERSION:', process.env.VUE_APP_VERSION)
+console.log('Build time:', new Date().toLocaleString())
